55218d94f39f01a8e993b1cac90df0a4f8335160
Valeria Ramey <valeriaramey@gmail.com>
2020-08-26 13:40:10 -0400

    guardian api completed, started working on search queries

--- app/services/api_article_fetcher.rb
+++ app/services/api_article_fetcher.rb
@@ -23,11 +23,21 @@ class ApiArticleFetcher
   end
 
   def fetch_articles
-   url= "https://content.guardianapis.com/search?order-by=oldest&page-Size=10&q=#{URI.encode(@query)}&api-key=a56dd13e-d0f2-40f4-992d-8cad04244c0d"
+   url= "https://content.guardianapis.com/search?order-by=oldest&page-Size=10&q=#{URI.encode(q)}&api-key=a56dd13e-d0f2-40f4-992d-8cad04244c0d"
+   puts url
    articles_serialized = open(url).read
    @articles = JSON.parse(articles_serialized)
   end
 
+  def q
+    query_arr = @query.map do |query|
+      "\"#{query}\""
+    end
+
+    query_arr.join(" AND ")
+
+
+  end
 
 
 end

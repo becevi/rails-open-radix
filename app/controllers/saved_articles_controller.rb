38d12a8037248fc0afb393fff070f9628c1f0b7e
Valeria Ramey <valeriaramey@gmail.com>
2020-08-27 15:17:30 -0400

    saved_articles if statment update

--- app/controllers/saved_articles_controller.rb
+++ app/controllers/saved_articles_controller.rb
@@ -3,9 +3,6 @@ class SavedArticlesController < ApplicationController
   def create
     @saved_article = SavedArticle.new(param[:article, :timeline]) # creating a link between timeline  and article
 
-     if @saved_article.save
-      redirect_to timeline_path
-    else
-     #maybe an error message
-  end
+     @saved_article.save
+     redirect_to timeline_path
 end
